e3c9cbb06ebd0c511e54cb62924d84cea040b33d
Mekenese Geneviève <neviimckfl@gmail.com>
2026-03-09 21:08:29 +1100

    feat: add route to manually insert bookmarks

--- src/Controller/BookmarkController.php
+++ src/Controller/BookmarkController.php
@@ -22,4 +22,31 @@ class BookmarkController extends AbstractController
             'BookmarkController' => $bookmarks,
         ]);
     }
+
+    #[Route("/bookmark/add", name: "bookmark_add")]
+    public function ajouterBookmark(EntityManagerInterface $entityManager): Response
+    {
+        $b1 = new Bookmark();
+        $b1->setUrl("https://symfony.com");
+        $b1->setCommentaire("Site officiel du framework PHP Symfony");
+        $b1->setDateCreation(new \DateTime());
+        
+        $b2 = new Bookmark();
+        $b2->setUrl("https://www.qwant.com");
+        $b2->setCommentaire("Moteur de recherche");
+        $b2->setDateCreation(new \DateTime());
+
+        $b3 = new Bookmark();
+        $b3->setUrl("https://github.com");
+        $b3->setCommentaire("Plateforme de versioning");
+        $b3->setDateCreation(new \DateTime());
+
+        $entityManager->persist($b1);
+        $entityManager->persist($b2);
+        $entityManager->persist($b3);
+
+        $entityManager->flush();
+
+        return new Response("Trois marque-pages ont été ajoutés avec succès !");
+    }
 }
